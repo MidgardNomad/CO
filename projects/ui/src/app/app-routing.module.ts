ec551ec86a8796d46aa892b405484e0790f2bc23
Amr Medhat <amr@programmingmap.com>
2024-03-16 14:38:33 +0200

    init ui

--- projects/ui/src/app/app-routing.module.ts
+++ projects/ui/src/app/app-routing.module.ts
@@ -1,7 +1,71 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () =>
+    import('./modules/landing-page/landing-page.module').then(
+      (m) => m.LandingPageModule,
+    ),
+  },
+  {
+    path:'auth',
+    loadChildren: () =>
+    import('./modules/auth/auth.module').then(
+      (m) => m.AuthModule,
+    ),
+  },
+  {
+    path:'profile',
+    loadChildren: () =>
+    import('./modules/profile/profile.module').then(
+      (m) => m.ProfileModule,
+    ),
+  },
+  {
+    path:'courses',
+    loadChildren: () =>
+    import('./modules/courses/courses.module').then(
+      (m) => m.CoursesModule,
+    ),
+  },
+  {
+    path:'dashboard',
+    loadChildren: () =>
+    import('./modules/dashboard/dashboard.module').then(
+      (m) => m.DashboardModule,
+    ),
+  },
+  {
+    path:'about',
+    loadChildren: () =>
+    import('./modules/about/about.module').then(
+      (m) => m.AboutModule,
+    ),
+  },
+  {
+    path:'contactUS',
+    loadChildren: () =>
+    import('./modules/contactus/contactus.module').then(
+      (m) => m.ContactusModule,
+    ),
+  },
+  {
+    path:'blogs',
+    loadChildren: () =>
+    import('./modules/blogs/blogs.module').then(
+      (m) => m.BlogsModule,
+    ),
+  },
+  {
+    path:'privacy',
+    loadChildren: () =>
+    import('./modules/privacy-policy/privacy-policy.module').then(
+      (m) => m.PrivacyPolicyModule,
+    ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
