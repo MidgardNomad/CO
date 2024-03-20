e2ffc914b01610ee102b195d8cc34b90e8bba932
Mahmoud Elebiare <33654362+HozayfaElebiare@users.noreply.github.com>
2024-03-21 01:24:50 +0200

    DAL CRUD Ops

--- projects/portal/src/app/app.component.ts
+++ projects/portal/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { DALService } from 'DAL';
+import { Component, inject } from '@angular/core';
+import { DALService, CrudService, Lesson } from 'DAL';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +9,38 @@ import { DALService } from 'DAL';
 export class AppComponent {
   title = 'portal';
 
-  constructor(private dalService: DALService) {
-    this.dalService.getData();
+  constructor(private crudService: CrudService) {
+    // set the collection name on calling the service
+    crudService.collectionName = 'admins';
+    // get all data
+    crudService.getData().then((data: any) => {
+      console.log(data);
+    }
+    );
+
+    this.getProfile();
+
+    // get all data
+    crudService.getData().then((data: any) => {
+      console.log(data);
+    }
+    );
+  }
+
+
+
+
+    getProfile(){
+    this.crudService.collectionName = 'profile';
+
+      // get single data
+      this.crudService.getSingleData('1').subscribe((data: any) => {
+        console.log(data);
+      });
+    }
+
+
+
   }
 
   
-}
