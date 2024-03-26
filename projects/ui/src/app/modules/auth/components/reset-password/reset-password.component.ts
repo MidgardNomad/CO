e45006404475bc953dc2f8f44cbe936542b13aca
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-03-26 21:47:07 +0200

    Forgot password responsive

--- projects/ui/src/app/modules/auth/components/reset-password/reset-password.component.ts
+++ projects/ui/src/app/modules/auth/components/reset-password/reset-password.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
+  timer: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.timer =
+      new Date(new Date().getTime() + 120000).getTime() - new Date().getTime();
+    const time = setInterval(() => {
+      if (this.timer <= 0) {
+        clearInterval(time);
+        return;
+      }
+      this.timer -= 1000;
+    }, 1000);
   }
-
 }
