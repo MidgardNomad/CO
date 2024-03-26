2c70987d1255d4df6421ebfb5eb20356a3a1a461
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-03-26 06:11:41 +0200

    Forget password screen Done.

--- projects/ui/src/app/modules/auth/components/verify-otp/verify-otp.component.ts
+++ projects/ui/src/app/modules/auth/components/verify-otp/verify-otp.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify-otp',
   templateUrl: './verify-otp.component.html',
-  styleUrls: ['./verify-otp.component.scss']
+  styleUrls: ['./verify-otp.component.scss'],
 })
 export class VerifyOtpComponent implements OnInit {
-
-  constructor() { }
+  otpCode: number;
+  countDown = 15;
+  isResendCodeActive = 0;
+  constructor(
+    private _location: Location,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const timer = setInterval(() => {
+      if (this.countDown === 0) {
+        clearInterval(timer);
+        this.isResendCodeActive = 100;
+        return;
+      }
+      this.countDown -= 1;
+    }, 1000);
   }
 
+  onGoBack() {
+    this._location.back();
+  }
+
+  onResendCode() {}
+
+  onOtpChange(f) {
+    this.otpCode = f;
+  }
+
+  onSubmit() {
+    console.log(this.otpCode);
+    this.router.navigate(['../reset-password'], { relativeTo: this.route });
+  }
 }
