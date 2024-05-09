50106b0ea8e3f41fcecc35ef213cb33a06bb8363
Mahmoud Elebiare <33654362+HozayfaElebiare@users.noreply.github.com>
2024-05-09 18:28:15 +0300

    Abdullah changes

---
+++ projects/ui/src/app/modules/profile-settings/profile-settings.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-settings',
+  templateUrl: './profile-settings.component.html',
+  styleUrls: ['./profile-settings.component.scss'],
+})
+export class ProfileSettingsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
