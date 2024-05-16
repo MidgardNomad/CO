32aad48832e8fcca100657709682d34a2312f4e5
Amr Medhat <amr@programmingmap.com>
2024-05-16 23:33:38 +0300

    send collectionName as a paramenter to crud services

--- projects/dal/src/lib/services/crud.service.ts
+++ projects/dal/src/lib/services/crud.service.ts
@@ -7,63 +7,62 @@ import { OrderByDirection } from 'firebase/firestore';
 })
 export class CrudService {
   // set the collection name on calling the service
-  collectionName: string = '';
+  // collectionName: string = '';
 
   constructor(private db: AngularFirestore) { 
-    this.collectionName = '';
-    if (this.collectionName == '') {
-      console.log('Please set the collection name in the service before calling any method.');
-    }
+    // this.collectionName = '';
+    // if (this.collectionName == '') {
+    //   console.log('Please set the collection name in the service before calling any method.');
+    // }
   }
 
   // get all data
-  async getData() {
-    this.db.collection(this.collectionName).snapshotChanges().subscribe((data: any) => {
+  async getData(collectionName:string) {
+    this.db.collection(collectionName).snapshotChanges().subscribe((data: any) => {
       console.log(data);
       return data;
      });
-
   }
 
   // add data
-  addData(data: any) {
-    return this.db.collection(this.collectionName).add(data);
+  addData(collectionName:string,data: any) {
+    return this.db.collection(collectionName).add(data);
   }
 
   // update data
-  updateData(id: any, data: any) {
-    return this.db.collection(this.collectionName).doc(id).update(data);
+  updateData(collectionName:string,id: any, data: any) {
+    return this.db.collection(collectionName).doc(id).update(data);
   }
 
   // delete data
-  deleteData(id: any) {
-    return this.db.collection(this.collectionName).doc(id).delete();
+  deleteData(collectionName:string,id: any) {
+    return this.db.collection(collectionName).doc(id).delete();
   }
 
   // get single data
-  getSingleData(id: any) {
-    return this.db.collection(this.collectionName).doc(id).get();
+  getSingleData(collectionName:string,id: any) {
+    return this.db.collection(collectionName).doc(id).get();
   }
 
   // get single data
-  getSingleDataByField(field: string, value: any) {
-    return this.db.collection(this.collectionName, ref => ref.where(field, '==', value)).snapshotChanges();
+  getSingleDataByField(collectionName:string,field: string, value: any) {
+    return this.db.collection(collectionName, ref => ref.where(field, '==', value)).snapshotChanges();
   }
 
   // get single data
-  getSingleDataByFieldWithLimit(field: string, value: any, limit: number) {
-    return this.db.collection(this.collectionName, ref => ref.where(field, '==', value).limit(limit)).snapshotChanges();
+  getSingleDataByFieldWithLimit(collectionName:string,field: string, value: any, limit: number) {
+    return this.db.collection(collectionName, ref => ref.where(field, '==', value).limit(limit)).snapshotChanges();
   }
 
   // get single data
-  getSingleDataByFieldWithOrder(field: string, value: any, orderField: string, order: OrderByDirection) {
-    return this.db.collection(this.collectionName, ref => ref.where(field, '==', value).orderBy(orderField, order)).snapshotChanges();
+  getSingleDataByFieldWithOrder(collectionName:string,field: string, value: any, orderField: string, order: OrderByDirection) {
+    return this.db.collection(collectionName, ref => ref.where(field, '==', value).orderBy(orderField, order)).snapshotChanges();
   }
 
 
   // get single data
-  getSingleDataByFieldWithOrderAndLimit(field: string, value: any, orderField: string, order: OrderByDirection, limit: number) {
-    return this.db.collection(this.collectionName, ref => ref.where(field, '==', value).orderBy(orderField, order).limit(limit)).snapshotChanges();
+  getSingleDataByFieldWithOrderAndLimit(collectionName:string,field: string, value: any, orderField: string, order: OrderByDirection, limit: number) {
+    return this.db.collection(collectionName, ref => ref.where(field, '==', value).orderBy(orderField, order).limit(limit)).snapshotChanges();
   }
 
   
