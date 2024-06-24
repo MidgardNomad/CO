c72f5a6c7c32bf765ddfafd13c745cb274babbbb
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-06-24 18:48:16 +0300

    finish profile (minus users courses - wait to add enroll functionality)

--- projects/portal/src/app/modules/profile/components/user-courses/user-courses.component.ts
+++ projects/portal/src/app/modules/profile/components/user-courses/user-courses.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-courses',
   templateUrl: './user-courses.component.html',
-  styleUrls: ['./user-courses.component.scss']
+  styleUrls: ['./user-courses.component.scss'],
 })
 export class UserCoursesComponent {
+  constructor(private router: Router) {}
 
+  navigateToCourses() {
+    this.router.navigate(['/courses/courses-list']);
+  }
 }
