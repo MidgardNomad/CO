688dd667664327ad07fdc4fe2c6a066e91dd86c9
osamaRomih <osamaromih2020@gmail.com>
2024-06-24 20:11:58 +0300

    new report

--- projects/portal/src/app/modules/about/about.module.ts
+++ projects/portal/src/app/modules/about/about.module.ts
@@ -5,9 +5,10 @@ import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import { FaqComponent } from './component/faq/faq.component';
 import { AboutPageComponent } from './component/about-page/about-page.component';
+import { ReportComponent } from './component/report/report/report.component';
 
 @NgModule({
-  declarations: [AboutComponent, FaqComponent, AboutPageComponent],
+  declarations: [AboutComponent, FaqComponent, AboutPageComponent, ReportComponent],
   imports: [CommonModule, AboutRoutingModule],
 })
 export class AboutModule {}
