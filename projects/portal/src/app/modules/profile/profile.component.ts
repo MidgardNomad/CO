df63ee04eac2f80a1c87971099f8d864a65ad5e6
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-06-24 17:39:39 +0300

    Add user streak to profile

--- projects/portal/src/app/modules/profile/profile.component.ts
+++ projects/portal/src/app/modules/profile/profile.component.ts
@@ -1,26 +1,7 @@
-import {
-  Component,
-  OnInit,
-  NgZone,
-  Renderer2 as Renderer,
-  ElementRef,
-  ViewChild,
-  AfterViewInit,
-} from '@angular/core';
-import {
-  ActivatedRoute,
-  Data,
-  Event as RouterEvent,
-  NavigationStart,
-  NavigationEnd,
-  NavigationError,
-  NavigationCancel,
-  Router,
-} from '@angular/router';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 import { User } from 'projects/dal/src/lib/models/user/user';
-import { loadingAnimation } from '../../shared/functions/loadingAnimation';
 import { AuthService } from 'DAL';
-import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -29,31 +10,14 @@ import { tap } from 'rxjs';
 })
 export class ProfileComponent implements OnInit {
   userDoc = <User>{};
-  week: string[] = [];
-  streakDay: string[] = [];
-  hasUserLoaded = false;
   showSettingsButton = false;
-  @ViewChild('mainProfileBody') mainProfileBody: ElementRef;
-  @ViewChild('spinner') spinner: ElementRef;
-  loadingAnimation = loadingAnimation();
   constructor(
     private route: ActivatedRoute,
-    private ngZone: NgZone,
-    private renderer: Renderer,
     private router: Router,
     private authService: AuthService
   ) {}
 
   ngOnInit(): void {
-    //View Streak Days!
-    let curr = new Date();
-    for (let i = 0; i < 7; i++) {
-      let first = curr.getDate() - curr.getDay() + i;
-      let day = new Date(curr.setDate(first)).toISOString().slice(0, 10);
-      this.week.push(day);
-    }
-    this.streakDay.push(this.week[0]);
-
     //Check if the user is viewing their own profile or visiting another student's profile!
     this.authService.user.subscribe((activeUser) => {
       if (activeUser.uid === this.route.snapshot.paramMap.get('uid')) {
@@ -64,9 +28,6 @@ export class ProfileComponent implements OnInit {
     //Get user data from resolver!
     this.route.data.subscribe((data: Data) => {
       this.userDoc = data['userData'];
-      if (this.userDoc.id == null) {
-        this.hasUserLoaded = false;
-      } else this.hasUserLoaded = true;
     });
   }
 
