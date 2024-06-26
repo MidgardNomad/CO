ca65d69d2ce304592c5c6df41434994238b41798
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-06-26 17:41:47 +0300

    update course model

--- projects/dal/src/lib/models/content/course.ts
+++ projects/dal/src/lib/models/content/course.ts
@@ -1,5 +1,9 @@
 export interface Course {
   id: string;
   title: string;
+  //this should be used to get the course from the route param
+  //instead of using the document ID
+  //Add it later because that might break a lot of things
+  name?: string;
   description: string;
 }
