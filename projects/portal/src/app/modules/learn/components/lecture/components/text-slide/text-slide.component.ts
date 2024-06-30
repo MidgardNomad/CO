31c6afd3602d1ba2b83b79ad332a6e36fcab25c7
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-06-30 15:49:00 +0300

    add text slide UI

--- projects/portal/src/app/modules/learn/components/lecture/components/text-slide/text-slide.component.ts
+++ projects/portal/src/app/modules/learn/components/lecture/components/text-slide/text-slide.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Ss } from 'DAL';
 
 @Component({
   selector: 'app-text-slide',
   templateUrl: './text-slide.component.html',
-  styleUrls: ['./text-slide.component.scss']
+  styleUrls: ['./text-slide.component.scss'],
 })
 export class TextSlideComponent {
-
+  @Input() slide: Ss;
 }
