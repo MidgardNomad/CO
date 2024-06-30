26055a771ed3888722002ef3527da6d70011af44
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-06-30 17:35:49 +0300

    add text-image slide UI and Logic

--- projects/portal/src/app/modules/learn/components/lecture/components/text-image-slide/text-image-slide.component.ts
+++ projects/portal/src/app/modules/learn/components/lecture/components/text-image-slide/text-image-slide.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Ss } from 'DAL';
 
 @Component({
   selector: 'app-text-image-slide',
   templateUrl: './text-image-slide.component.html',
-  styleUrls: ['./text-image-slide.component.scss']
+  styleUrls: ['./text-image-slide.component.scss'],
 })
 export class TextImageSlideComponent {
-
+  @Input() slide: Ss;
 }
