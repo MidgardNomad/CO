6e6676964b26cf284bd99e475aa904b9d2f88030
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-07-01 10:52:29 +0300

    remove repetative code in connected-accounts component

--- projects/portal/src/app/modules/profile-settings/components/connected-accounts/connected-accounts.component.ts
+++ projects/portal/src/app/modules/profile-settings/components/connected-accounts/connected-accounts.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConnectedAccounts, UsersService } from 'DAL';
+import { take } from 'rxjs';
+
+//Type: Connected Accounts Object
 
 @Component({
   selector: 'app-connected-accounts',
   templateUrl: './connected-accounts.component.html',
-  styleUrls: ['./connected-accounts.component.scss']
+  styleUrls: ['./connected-accounts.component.scss'],
 })
-export class ConnectedAccountsComponent {
+export class ConnectedAccountsComponent implements OnInit {
+  connectedAccounts: ConnectedAccounts[] = [];
+  accountTemplates: { website: string; example: string }[] = [
+    {
+      website: 'LinkedIn',
+      example: 'https://www.linkedin.com/in/username',
+    },
+    {
+      website: 'GitHub',
+      example: 'https://github.com/username',
+    },
+  ];
 
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.usersService
+      .getUserID()
+      .pipe(take(1))
+      .subscribe((userID) => {
+        this.usersService.getSingleUser(userID).subscribe((user) => {
+          this.connectedAccounts = user.connectedAccounts;
+          console.log(this.connectedAccounts);
+        });
+      });
+  }
+
+  onConnect() {}
 }
