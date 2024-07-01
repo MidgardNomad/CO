fbc60bacd47c2009674a8e8156dfa52a7c587e6f
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-07-01 17:28:05 +0300

    add style and functionality to the fill slide component

--- projects/portal/src/app/modules/learn/components/lecture/components/fill-slide/fill-slide.component.ts
+++ projects/portal/src/app/modules/learn/components/lecture/components/fill-slide/fill-slide.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Ss } from 'DAL';
 
 @Component({
   selector: 'app-fill-slide',
   templateUrl: './fill-slide.component.html',
-  styleUrls: ['./fill-slide.component.scss']
+  styleUrls: ['./fill-slide.component.scss'],
 })
 export class FillSlideComponent {
+  isAnswerWrong = false;
+  isAnswerCorrect = false;
+  @Input() slide: Ss;
+  @Output() next = new EventEmitter();
 
+  onCheck(qForm: NgForm) {
+    if (qForm.value.answer === this.slide.qAnswer) {
+      this.isAnswerCorrect = true;
+    } else {
+      this.isAnswerWrong = true;
+    }
+  }
+
+  continue() {
+    this.next.emit();
+  }
 }
