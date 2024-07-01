5a89523cd0bcd2d3bfb616e6bc0ef624bea841bd
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-07-01 17:28:47 +0300

    add style and functionality to the mcq slide component

--- projects/portal/src/app/modules/learn/components/lecture/components/mcq-slide/mcq-slide.component.ts
+++ projects/portal/src/app/modules/learn/components/lecture/components/mcq-slide/mcq-slide.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Ss } from 'DAL';
 
 @Component({
   selector: 'app-mcq-slide',
   templateUrl: './mcq-slide.component.html',
-  styleUrls: ['./mcq-slide.component.scss']
+  styleUrls: ['./mcq-slide.component.scss'],
 })
 export class McqSlideComponent {
+  isAnswerCorrect = false;
+  isAnswerWrong = false;
+  @Input() slide: Ss;
+  @Output() continue = new EventEmitter();
 
+  onCheckAnswer(form: NgForm) {
+    if (
+      this.slide.options.indexOf(form.value.option) === this.slide.mcqAnswer
+    ) {
+      this.isAnswerCorrect = true;
+    } else {
+      console.log('wrong');
+      this.isAnswerWrong = true;
+    }
+  }
+
+  onContinue() {
+    this.continue.emit();
+  }
 }
