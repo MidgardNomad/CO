18a5efc3271a670087e6b1470f181ef02eabd5aa
M-E-Taha <elsha3rawy.btm@gmail.com>
2024-07-23 12:10:17 +0300

    work from the mentors module and remove the mentors list module

--- projects/admin/src/app/modules/mentors/mentors.component.ts
+++ projects/admin/src/app/modules/mentors/mentors.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Mentor } from 'DAL';
+import * as moment from 'moment-timezone';
+import { MentorDialogComponent } from '../../modal/mentor-dialog/mentor-dialog.component';
 
 @Component({
   selector: 'app-mentors',
   templateUrl: './mentors.component.html',
-  styleUrls: ['./mentors.component.scss']
+  styleUrls: ['./mentors.component.scss'],
 })
-export class MentorsComponent {
+export class MentorsComponent implements OnInit {
+  dataSource: Mentor[] = [
+    {
+      name: 'Mahmoud',
+      timeZone: moment.tz.guess(),
+      birthdate: new Date(),
+      experience: 1.5,
+      expertise: ['Flutter', 'Angular', 'Express'],
+      id: 'id',
+      linkedInLink: 'www.linkedin.com',
+      profilePicture: 'test',
+      weeklySchedule: null,
+    },
+  ];
 
+  displayedColumns = ['name', 'age', 'experience', 'timezone'];
+
+  constructor(private matDialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
+  onAddMentor() {
+    this.matDialog.open(MentorDialogComponent, {
+      width: '700px',
+      data: {
+        header: 'Add new Mentor',
+      },
+    });
+  }
+
+  navigateToMentor(data) {
+    console.log(data);
+  }
 }
