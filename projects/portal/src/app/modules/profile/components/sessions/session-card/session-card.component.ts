acea5a6a8854f005bfb1a123a7923528b3a26ed3
Amr Medhat <amr@programmingmap.com>
2024-08-05 08:09:41 +0300

    convert session time based onuser TZ

--- projects/portal/src/app/modules/profile/components/sessions/session-card/session-card.component.ts
+++ projects/portal/src/app/modules/profile/components/sessions/session-card/session-card.component.ts
@@ -16,11 +16,14 @@ export class SessionCardComponent implements OnInit {
   constructor(private sessionService: SessionService) {}
 
   getTimeAndDate(time: string, dow: number) {
-    momenttz.tz.setDefault('Africa/Cairo');
     let day = new Date();
     day.setDate(day.getDate() + ((dow + (7 - day.getDay())) % 7));
-    day.setHours(+time.split(':')[0]);
-    day.setMinutes(+time.split(':')[1]);
+
+
+    let userTime=this.convertTime(time,'Africa/Cairo','America/Denver');
+
+    day.setHours(+userTime.split(':')[0]);
+    day.setMinutes(+userTime.split(':')[1]);
     day.setSeconds(0);
 
     if (day.getDate() === new Date().getDate()) {
@@ -39,9 +42,26 @@ export class SessionCardComponent implements OnInit {
     this.sessionService.getDate().subscribe((dates) => {
       this.dates = dates;
     });
+    this.getUserTimeZone();
   }
 
   bookSession(session: Session) {
     const bookedSession: BookedSession = {} as BookedSession;
+  }
+
+  convertTime(time:string,sessionTZ:string,userTZ:string){
+
+    const cairoTime = moment.tz(time, 'HH:mm', sessionTZ);
+
+    // Convert to user Time (ET)
+    const easternTime = cairoTime.clone().tz(userTZ);
+
+    return easternTime.format('HH:mm');
+
+  }
+
+  getUserTimeZone(): void {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log('User Time Zone:', userTimeZone);
   }
 }
